5f3e235f241f0e792fe5bf29319ef56fd251d9b8
hw <eazyeazy@126.com>
2018-08-08 22:23:35 +0800

    20180808

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./app.css";
-import ReactImage from "./react.png";
+import ReactImage from './react.png';
 
 export default class App extends Component {
   constructor(props) {
@@ -9,7 +9,7 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    fetch("/api/getUsername")
+    fetch('api/getUsername')
       .then(res => res.json())
       .then(user => this.setState({ username: user.username }));
   }
@@ -18,7 +18,8 @@ export default class App extends Component {
     return (
       <div>
         {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
+          <h1> Hello{' '}{this.state.username}
+          </h1>
         ) : (
           <h1>Loading.. please wait!</h1>
         )}
